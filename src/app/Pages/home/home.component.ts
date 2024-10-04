c42f7af97268b514cf04015082fcd19450212c53
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-04 11:34:52 +0200

    added logic to get the api info on the home page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,12 +1,38 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+     public data:any;
+     public info:any;
+     constructor(private http: HttpClient){}
+     title ='api2'
+  ngOnInit(): void {
+    this.fetchDetails;
+  }
 
+   fetchDetails(){
+    this.http.get('')
+    .subscribe((res:any)=>{
+      console.log();
+      this.data =res;
+    })
+
+    this.http.get('')
+    .subscribe((res:any)=>{
+      console.log();
+      this.info =res;
+    })
+
+   }
+
+   
 }
