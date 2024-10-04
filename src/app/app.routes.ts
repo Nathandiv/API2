735b793a65cb0ad83f190306bd5810552f26d816
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-04 10:53:12 +0200

    added routing for the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { LandingpageComponent } from './Pages/landingpage/landingpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'landing',component:LandingpageComponent}
+
+];
